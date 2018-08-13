42cc60571c84a604b54f8c2ab38e606a95aaf0e1
tomohikoseven <tomohikoseven0905@gmail.com>
2018-08-13 20:59:28 +0900

    Part2

---
+++ 速習Vue.js/Part2/sample04.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: '<h1>こんにちは</h1>' +
+              '<span><a href="https://jp.vuejs.org/">Vue.js!</a></span>'
+  }
+});
---
+++ 速習Vue.js/Part2/sample08.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    email: 'Yamada@example.com'
+  },
+  computed: {
+    localEmail: function() {
+      return this.email.split('@')[0].toLowerCase();
+    }
+  }
+});
+
